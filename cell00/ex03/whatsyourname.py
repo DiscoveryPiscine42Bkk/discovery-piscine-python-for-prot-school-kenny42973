90a0a0848fc548e144c3e176a84ce83d326a07b2
kenny42973 <kenkuy500@gmail.com>
2025-03-10 13:17:03 +0700

    Add files via upload

---
+++ cell00/ex03/whatsyourname.py
@@ -0,0 +1,7 @@
+first_name = 'Silvanus'
+last_name = 'Jaithan'
+whole_name = first_name + ' ' + last_name
+
+print(f"Hey, what's your first name? : {first_name}") 
+print(f'And your last name? : {last_name}') 
+print(f'Well, pleased to meet you, {whole_name}.')
