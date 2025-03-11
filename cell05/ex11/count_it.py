146792b4b44de223d328c1c99c80d81a26f7fc26
kenny42973 <kenkuy500@gmail.com>
2025-03-11 14:11:54 +0700

    Update count_it.py

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -2,6 +2,6 @@ word = input().split()
 if len(word) == 0:
     print('none')
 else:
-    print(len(word))
+    print(f'parameters: {len(word)}')
     for x in range(len(word)):
         print(f'{word[x]}: {len(word[x])}')
