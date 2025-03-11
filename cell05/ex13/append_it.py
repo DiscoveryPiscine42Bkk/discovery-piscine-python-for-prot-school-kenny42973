f933275bf37bc6b573a0f63c5a12c4df798cd9e5
kenny42973 <kenkuy500@gmail.com>
2025-03-11 09:45:03 +0700

    Add files via upload

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,9 @@
+word = input().split()
+if len(word) == 0:
+    print('none')
+else:
+    words = []
+    for x in range(len(word)):
+        if not 'ism' in word[x]:
+            words.append(word[x] + 'ism')
+print(*words) 
