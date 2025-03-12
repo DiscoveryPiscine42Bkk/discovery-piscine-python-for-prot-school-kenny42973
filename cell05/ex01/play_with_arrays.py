664f1003d354b6f79e41d74bf3da52794766039c
kenny42973 <kenkuy500@gmail.com>
2025-03-12 14:53:47 +0700

    Update play_with_arrays.py

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,7 +1,9 @@
 og_array = [2, 8, 9, 48, 8, 22,-12, 2]
-New_array = og_array.copy()
-for x in range(len(og_array)):
-    New_array[x] = New_array[x] + 2
-
+New_array = []
+x = 0
+while x < 8:
+    result = og_array[x] + 2
+    New_array.append(result)
+    x += 1
 print(f'Original array: {og_array}')
 print(f'New array: {New_array}')
