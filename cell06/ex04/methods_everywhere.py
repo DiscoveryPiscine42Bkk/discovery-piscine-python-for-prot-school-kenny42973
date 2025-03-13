12b67d165236e746ebefcbd3b0229429dd0c4e8b
kenny42973 <kenkuy500@gmail.com>
2025-03-13 09:53:59 +0700

    Update methods_everywhere.py

--- cell06/ex04/methods_everywhere.py
+++ cell06/ex04/methods_everywhere.py
@@ -1,10 +1,14 @@
-word = input().split(' ')
-word = [text.replace('"', '').replace("'", '') for text in word]
+word = input().split("'")
+while '' in word:
+    word.remove('')
+while ' ' in word:
+    word.remove(' ')
 if len(word) == 0:
     print('none')
 else:
     only_8 = []
-    for x in range(len(word)):
+    x = 0
+    while x < len(word):
         if len(word[x]) < 8:
             lower_8 = 8 - len(word[x])
             only_8.append(word[x] + ('Z'*lower_8))
@@ -12,4 +16,7 @@ else:
             only_8.append(word[x][0:8])        
         else:
             only_8.append(word[x])
+        x += 1
     print(*only_8, sep='\n')
+
+#'lol' 'physically' 'backpack' 
